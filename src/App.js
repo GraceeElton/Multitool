87cd61c41a74c5b59c4df9935f1b1307ff74e2e3
Gracee Elton <graceeelton@gmail.com>
2022-08-17 14:18:03 -0600

    Add my name to the website

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi TPS TEAM!! 
+          Gracee Elton 
         </p>
         <a
           className="App-link"
